931fedbc691bc38a4f704a08c8e230e9e3cf63d5
Zachary V <zachary.veillette@gmail.com>
2021-05-10 16:56:06 -0400

    *Changed cycle winner for TT modifications

--- jars/src/data/scripts/hullmods/Muleships_DsRacerTriTachyon.java
+++ jars/src/data/scripts/hullmods/Muleships_DsRacerTriTachyon.java
@@ -4,19 +4,16 @@ import com.fs.starfarer.api.combat.BaseHullMod;
 import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
 
-public class Muleships_CycleWinner extends BaseHullMod {
+public class Muleships_DsRacerTriTachyon extends BaseHullMod {
 
 	private static final float SUPPLY_USE_MULT = 2f;
-	private static final float SPEED_BOOST = 10f;
 	
 	public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
 		stats.getSuppliesPerMonth().modifyMult(id, SUPPLY_USE_MULT);
-		stats.getMaxSpeed().modifyFlat(id, SPEED_BOOST);
 	}
 	
 	public String getDescriptionParam(int index, HullSize hullSize) {
-		if (index == 0) return "" + SPEED_BOOST;
-		if (index == 1) return "" + (int)((SUPPLY_USE_MULT - 1f) * 100f) + "%";
+		if (index == 0) return "" + (int)((SUPPLY_USE_MULT - 1f) * 100f) + "%";
 		return null;
 	}
 }
