0830269e6d3c3165f9908fdc166b92b1084de3d4
Zachary V <zachary.veillette@gmail.com>
2021-05-11 20:54:32 -0400

    +spawn for dsracer freighter

--- jars/src/data/scripts/hullmods/Muleships_DsRacerFreighter.java
+++ jars/src/data/scripts/hullmods/Muleships_DsRacerFreighter.java
@@ -5,18 +5,20 @@ import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
 
 public class Muleships_DsRacerFreighter extends BaseHullMod {
-	private static final float CARGO_CAPACITY_MULT = 3f;
-	private static final float FUEL_CAPACITY_MULT = 1.6f;
+	private static final float CARGO_CAPACITY = 20f;
+	private static final float FUEL_CAPACITY = 10f;
 	private static final float MINIMUM_CREW = 14f;
 	private static final float MAX_CREW = 15f;
+	private static final float BURN_LEVEL = -1f;
 	private static final float MAX_SPEED_MULT = 0.8f;
 	private static final float MAX_TURN_RATE = 0.8f;
 	
 	public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
-		stats.getFuelMod().modifyMult(id, FUEL_CAPACITY_MULT);
-		stats.getCargoMod().modifyMult(id, CARGO_CAPACITY_MULT);
+		stats.getFuelMod().modifyFlat(id, FUEL_CAPACITY);
+		stats.getCargoMod().modifyFlat(id, CARGO_CAPACITY);
 		stats.getMinCrewMod().modifyFlat(id, MINIMUM_CREW);
 		stats.getMaxCrewMod().modifyFlat(id, MAX_CREW);
+		stats.getMaxBurnLevel().modifyFlat(id, BURN_LEVEL);
 		stats.getMaxSpeed().modifyMult(id, MAX_SPEED_MULT);
 		stats.getMaxTurnRate().modifyMult(id, MAX_TURN_RATE);
 	}
