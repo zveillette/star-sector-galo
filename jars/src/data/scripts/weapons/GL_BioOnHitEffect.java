d81d511e8813177d80ce827d395218932c7d3400
Zachary V <zachary.veillette@gmail.com>
2022-02-20 16:32:20 -0500

    +bio effect watcher

--- jars/src/data/scripts/weapons/GL_BioOnHitEffect.java
+++ jars/src/data/scripts/weapons/GL_BioOnHitEffect.java
@@ -8,23 +8,27 @@ import com.fs.starfarer.api.combat.DamagingProjectileAPI;
 import com.fs.starfarer.api.combat.OnHitEffectPlugin;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.listeners.ApplyDamageResultAPI;
+import com.fs.starfarer.api.util.IntervalUtil;
 
 import org.apache.log4j.Logger;
 import org.lwjgl.util.vector.Vector2f;
 
 public class GL_BioOnHitEffect extends BaseCombatLayeredRenderingPlugin implements OnHitEffectPlugin {
 
+    private final static String CUSTOM_DATA_CR_IMPACT = "GL_BIO_CR_IMPACT";
     private final static Logger logger = Global.getLogger(GL_BioOnHitEffect.class);
 
-    private DamagingProjectileAPI proj;
     private ShipAPI target;
-    private int test = 10;
+    private Integer crImpact = 5;
+    private IntervalUtil interval;
 
     public GL_BioOnHitEffect() {}
 
-    public GL_BioOnHitEffect(DamagingProjectileAPI proj, ShipAPI target) {
-        this.proj = proj;
+    public GL_BioOnHitEffect(ShipAPI target) {
         this.target = target;
+
+        interval = new IntervalUtil(2f, 3f);
+		interval.forceIntervalElapsed();
     }
 
     public void init(CombatEntityAPI entity) {
@@ -36,12 +40,17 @@ public class GL_BioOnHitEffect extends BaseCombatLayeredRenderingPlugin implemen
             return;
         }
 
-        logger.info("hello");
-        --test;
+        crImpact = (Integer) target.getCustomData().get(CUSTOM_DATA_CR_IMPACT);
+        interval.advance(amount);
+        if (interval.intervalElapsed()) {
+            interval.setElapsed(0);
+            target.setCustomData(CUSTOM_DATA_CR_IMPACT, --crImpact);
+            logger.info("CR DEGRADE: " + crImpact);
+		}
     }
 
     public boolean isExpired() {
-        return test == 0;
+        return crImpact == 0 || !target.isAlive() || !Global.getCombatEngine().isEntityInPlay(target);
     }
 
     @Override
@@ -56,7 +65,14 @@ public class GL_BioOnHitEffect extends BaseCombatLayeredRenderingPlugin implemen
             return;
         }
 
-        GL_BioOnHitEffect effect = new GL_BioOnHitEffect(proj, (ShipAPI) target);
+        // Make sure we only have 1 watcher for bio cr degradation
+        Integer crImpact = (Integer) target.getCustomData().get(CUSTOM_DATA_CR_IMPACT);
+        if (crImpact != null) {
+            target.setCustomData(CUSTOM_DATA_CR_IMPACT, crImpact + 5);
+        }
+
+        target.setCustomData(CUSTOM_DATA_CR_IMPACT, 5);
+        GL_BioOnHitEffect effect = new GL_BioOnHitEffect((ShipAPI) target);
         CombatEntityAPI entity = engine.addLayeredRenderingPlugin(effect);
         entity.getLocation().set(proj.getLocation());
     }
